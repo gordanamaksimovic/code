d5b1bed4e196f5fd6c121505756587ce8ccb02c5
gordanamaksimovic <61363417+gordanamaksimovic@users.noreply.github.com>
2022-06-29 10:05:01 +0200

    Update ManageController.cs

--- ManageController.cs
+++ ManageController.cs
@@ -128,55 +128,6 @@ namespace MvcApp.Controllers
         // POST: /Manage/SetPassword
 
 
-        ////GET: /Manage/ManageLogins
-        //[HttpGet]
-        //public async Task<IActionResult> ManageLogins(ManageMessageId? message = null)
-        //{
-        //    ViewData["StatusMessage"] =
-        //        message == ManageMessageId.RemoveLoginSuccess ? "The external login was removed."
-        //        : message == ManageMessageId.AddLoginSuccess ? "The external login was added."
-        //        : message == ManageMessageId.Error ? "An error has occurred."
-        //        : "";
-        //    var user = await GetCurrentUserAsync();
-        //    if (user == null)
-        //    {
-        //        return View("Error");
-        //    }
-        //    var userLogins = await _userManager.GetLoginsAsync(user);
-        //    var otherLogins = _signInManager.GetExternalAuthenticationSchemes().Where(auth => userLogins.All(ul => auth.AuthenticationScheme != ul.LoginProvider)).ToList();
-        //    ViewData["ShowRemoveButton"] = user.PasswordHash != null || userLogins.Count > 1;
-        //    return View(new ManageLoginsViewModel
-        //    {
-        //        CurrentLogins = userLogins,
-        //        OtherLogins = otherLogins
-        //    });
-        //}
-
-        ////
-        // GET: /Manage/LinkLoginCallback
-        //[HttpGet]
-        //public async Task<ActionResult> LinkLoginCallback()
-        //{
-        //    var user = await GetCurrentUserAsync();
-        //    if (user == null)
-        //    {
-        //        return View("Error");
-        //    }
-        //    var info = await _signInManager.GetExternalLoginInfoAsync(await _userManager.GetUserIdAsync(user));
-        //    if (info == null)
-        //    {
-        //        return RedirectToAction(nameof(ManageLogins), new { Message = ManageMessageId.Error });
-        //    }
-        //    var result = await _userManager.AddLoginAsync(user, info);
-        //    var message = ManageMessageId.Error;
-        //    if (result.Succeeded)
-        //    {
-        //        message = ManageMessageId.AddLoginSuccess;
-        //        // Clear the existing external cookie to ensure a clean login process
-        //        await HttpContext.Authentication.SignOutAsync(_externalCookieScheme);
-        //    }
-        //    return RedirectToAction(nameof(ManageLogins), new { Message = message });
-        //}
 
 
         protected override void Dispose(bool disposing)
